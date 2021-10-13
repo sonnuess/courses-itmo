8bd160ad6036b48b2bc92799423e4a21208da0ab
Sofya Kozhanova <kozhanova_28@mail.ru>
2021-10-13 18:38:38 +0300

    new commit2

--- 01-Classes/main.py
+++ 01-Classes/main.py
@@ -8,23 +8,31 @@ class Person:
         print(f'hi {self.name}!')
 
 
-    def __del__(self):
+    def __str__(self):
+        return f'Person<name={self.name}>'
+    def __eq__(self, other):
+        return self.name == other.name
 
 
 
 me = Person('Nick')
+clone = Person('Nick')
 you = Person('Vasya')
 
 print(me.arms_count, you.arms_count)
 
-me.arms_count = 5
+Person.arms_count = 5
 print(me.arms_count, you.arms_count)
 
 
-print(me.name, you.name)
+print(me, you)
+print (me== clone)
 me.greet()
 you.greet()
 
 me.age = 23
 you.age = 45
 print(me.age, you.age)
+
+
+
