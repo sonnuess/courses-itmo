8dfbab7391bffe797b312592aff394edf2797d0d
Sofya Kozhanova <kozhanova_28@mail.ru>
2021-11-23 22:40:15 +0300

    homework 24/11/21

---
+++ product.py
@@ -0,0 +1,19 @@
+def time_it(func):
+    def how_long(*args, **kwargs):
+        import time
+        tic = time.thread_time()
+        func(*args, **kwargs)
+        toc = time.thread_time()
+        f"lasts {toc - tic} second"
+    return how_long()
+
+
+@time_it
+def product(a, *args) -> int:
+    result = a
+    for i in args:
+        result *= i
+    return result
+
+
+print(product(1, 2, 3, 4))
