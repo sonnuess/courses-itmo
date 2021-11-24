f15bca7c57511c8718b6ae8ada27e1eae35079d7
Sofya Kozhanova <kozhanova_28@mail.ru>
2021-11-24 18:35:58 +0300

    all

---
+++ generator/multipliers.py
@@ -0,0 +1,18 @@
+from typing import Generator
+
+def simple():
+    simpp =
+    while True:
+        if n / 0 == int:
+            multi = i + '*'
+
+
+
+
+
+
+
+
+
+
+n = int(input())
---
+++ generator/repeater-gen.py
@@ -0,0 +1,30 @@
+from typing import Any, Generator
+
+
+def repeater(value: Any) -> Generator:
+    while True:
+        yield value
+
+
+#for item in repeater(5):
+#    print(5)
+
+# Импортируем класс Iterator для аннотации
+from collections.abc import Iterator    # либо ->  from typing import Any, Generator
+
+
+# Функция-генератор для степеней двойки
+def pow_twelve(max_pow: int) -> Iterator:
+    cur_pow = 0
+
+    while cur_pow <= max_pow:
+        # Вот тут и происходит магия: когда функция
+        # будет вызвана повторно, она начнётся не с начала,
+        # а со строчки после yield
+        yield 12 ** cur_pow
+        cur_pow += 1
+
+
+# Выводим степени двойки от 2^0 до 2^10
+for i in pow_twelve(5):
+    print(i)
